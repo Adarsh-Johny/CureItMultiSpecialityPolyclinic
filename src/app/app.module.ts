da38d4f466035f0475d740f7fd3fd2fa0f25e800
Adarsh Johny <Adarshjohny@carestack.com>
2023-02-18 17:17:39 +0530

    removed unwanted files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainPageComponent } from './main-page/main-page.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -16,8 +15,7 @@ registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
